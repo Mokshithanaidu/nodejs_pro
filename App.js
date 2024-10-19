ef6a015fa1fb3640e83b79ade2762e2c05876955
imokshithanaidu <imokshithanaidu@gmail.com>
2024-10-19 14:33:17 +0530

    first done

---
+++ App.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+const port=3000;
+p.get("/", (req, res)=>
+{
+res.send("<h1>hello world</h1>");
+});
+app.listen(port, ()=>
+{
+console.log(app listening to the port ${port});
+}
+)
